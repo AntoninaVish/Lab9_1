5e60d85a12058927ca1184c61dd4a4e5defadf28
AntoninaVish <vishivtonya@gmail.com>
2021-12-11 12:26:09 +0300

    Добавьте файлы проекта.

---
+++ Lab9_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                
+                    int x;//first num
+                    int y;// second num
+                    double z;//resul
+                    int opr; //code operation
+            try
+            {
+                Console.WriteLine("Вас приветствует калькулятор!");
+                Console.Write("Введите целое число. X=");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите целое число. Y=");
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            
+            Console.WriteLine("Введите код операции: \n1 - сложение \n2 - вычитание \n3 - произведение \n4 - частное");
+            Console.Write("Ваш выбор:");
+            try
+            {
+                opr = Convert.ToInt32(Console.ReadLine());
+            
+                switch (opr)
+                    {
+                        case 1:
+                            z = x + y;
+                            Console.WriteLine("Результат = {0}", z);
+                            break;
+                        case 2:
+                            z = x - y;
+                            Console.WriteLine("Результат = {0}", z);
+                            break;
+                        case 3:
+                            z = x * y;
+                            Console.WriteLine("Результат = {0}", z);
+                            break;
+                        case 4:
+                            z = (double) x / y;
+                            Console.WriteLine("Результат = {0}", z);
+                            break;
+                    default:
+                        Console.WriteLine("Ошибка. Нет операции с выбранным номером.");
+                        break;
+                    }
+               
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка. Некорректный ввод.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Деление на 0!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab9__1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int x;//first num
+                int y;// second num
+                double z=0;//resul
+                int [] opr = {1,2,3,4}; //code operation
+            try
+            {
+                Console.WriteLine("Вас приветствует калькулятор!");
+                Console.Write("Введите целое число. X=");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите целое число. Y=");
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+                
+            }
+            Console.WriteLine("Введите код операции: \n1 - сложение \n2 - вычитание \n3 - произведение \n4 - частное");
+
+                Console.Write("Ваш выбор:");
+              int codeOpr;
+            try
+            {
+                codeOpr = Convert.ToInt32(Console.ReadLine());
+                int code = opr[-1 + codeOpr];
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка. Некорректный ввод.");
+                Console.ReadKey();
+                return;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка. Нет операции с выбранным номером.");
+                Console.ReadKey();
+                return;
+            }
+            try
+            {
+
+                if (codeOpr == 1)
+                {
+                z = x + y;
+                }
+                else if (codeOpr == 2)
+                {
+                z = x - y;
+                }
+                else if (codeOpr == 3)
+                {
+                z = x * y;
+                }
+                else if (codeOpr == 4)
+                {  
+                z=x / y; 
+                }
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Результат = {0}", z);
+
+            Console.ReadKey();
+            
+           
+        }
+    }   
+
+}
